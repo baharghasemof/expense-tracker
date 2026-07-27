2b2c7dfd17fd96403ffa59dacd0103765c09578f
Bahar ghasem of <bahar2009gasemof@gmail.com>
2026-07-27 23:43:42 +0330

    Version 1.0: Created Expense Tracker

---
+++ expense_tracker.py
@@ -0,0 +1,8 @@
+name = input("Expense name: ")
+amount = float(input("Amount: "))
+category = input("Category: ")
+
+print("\n=== Expense information ===")
+print(f"name        : {name}")
+print(f"Amount      : {amount}")
+print(f"Category    : {category}")
