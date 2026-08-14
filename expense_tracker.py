f16f3d3865c5c347bc6cdaf66c135c7ded210766
Bahar ghasem of <bahar2009gasemof@gmail.com>
2026-08-14 23:38:51 +0330

    Upgrade Expense Tracker to Version 2.0 with OOP

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,24 +1,50 @@
-expenses = [ ]
+class Expense:
+    def __init__(self, name, amount, category):
+        self.name = name
+        self.amount = amount
+        self.category = category
+
+    def show_info(self):
+        print("Expense Name:", self.name)
+        print("Amount:", self.amount)
+        print("Category:", self.category)
+
+    def is_expensive(self):
+        return self.amount >= 50000
+
+    def apply_discount(self):
+        return self.amount * (1 - 50 / 100)
+
+    def change_category(self, new_category):
+        self.category = new_category
+
+
+expenses = []
 
 name = input("Expense name: ")
 amount = float(input("Amount: "))
 category = input("Category: ")
 
-expense = [name, amount, category]
-
+expense = Expense(name, amount, category)
 expenses.append(expense)
 
-print("\nlast expense: ")
-print(expenses[-1])
+print("\nLast expense:")
+expenses[-1].show_info()
 
-print("\n=== Expense information ===")
-print(f"name        : {name}")
-print(f"Amount      : {amount}")
-print(f"Category    : {category}")
+print("\n=== Testing Methods ===")
 
-print("\ntotal expenses recorded: ", len(expenses))
+print("Is expensive:", expense.is_expensive())
+
+print("Amount after 50% discount:", expense.apply_discount())
+
+expense.change_category("Shopping")
+
+print("\n=== Updated Expense ===")
+expense.show_info()
+
+print("\nTotal expenses recorded:", len(expenses))
 
 print("\n=== All Expenses ===")
 
 for expense in expenses:
-    print(expense)
+    expense.show_info()
